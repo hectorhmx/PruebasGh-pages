d8f221aa99ae877a61e39b827602f6384bc69074
unknown <hectorhmx.55@gmail.com>
2018-05-11 00:26:08 -0500

    Pagina Prueba

---
+++ PaginaMaterialize/js/app.js
@@ -0,0 +1,8 @@
+  $(document).ready(function(){
+
+    $('.parallax').parallax();
+    $(".dropdown-trigger").dropdown();
+    $('.sidenav').sidenav();
+      $('.pushpin').pushpin();
+        $('.slider').slider();
+  });
